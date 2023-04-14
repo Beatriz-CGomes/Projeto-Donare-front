ce4d94ab30b7822ae4f6d63ab156d54ca97cc540
GabiSilvaBelo <gabybelosp@gmail.com>
2023-04-14 15:29:10 -0300

    Footer

---
+++ src/components/estaticos/footer/Footer.tsx
@@ -0,0 +1,27 @@
+
+import React from 'react';
+import './Footer.css';
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <div className="container">
+        <div className="row">
+          <div className="col-md-12">
+            <ul className="lista">
+              <li className="lista-item"><a href="#">Termos de Serviço</a></li>
+              <li className="lista-item"><a href="#">Diretrizes da Comunidade</a></li>
+              <li className="lista-item"><a href="#">Sobre</a></li>
+              <li className="lista-item"><a href="#">Dúvidas</a></li>
+            </ul>
+            <div className="nome">
+              Donare Corporation © 2023
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
