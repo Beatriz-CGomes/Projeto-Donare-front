d06c5e0db458c247ce549e55aaba6f6d41ff32ed
JoasChoque <joaschoque@hotmail.com>
2023-04-14 15:29:00 -0300

    Page Home

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 import Navbar from './components/estaticos/navbar/Navbar'
@@ -11,7 +9,6 @@ function App() {
   return (
     <>
     <Navbar/>
-    
     </>
   );
 }
