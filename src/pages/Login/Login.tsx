86ad76c53350aa863f3161f2a3441d166f88e61e
Flah_aga <fl.lindinha2014@gmail.com>
2023-04-19 12:44:03 -0300

    Alterações do login.tsx

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,33 +1,79 @@
-import React from 'react';
+import React, { ChangeEvent, useEffect, useState } from 'react';
 import { Grid, Typography, TextField, Button } from '@material-ui/core';
 import { Box } from '@mui/material';
-import { Link } from 'react-router-dom';
+import { Link,useNavigate } from 'react-router-dom';
+import { login } from '../../services/Service';
+
+import useLocalStorage from 'react-use-localstorage';
+import UserLogin from '../../models/UserLogin';
+
 import './Login.css';
 
 
 function Login() {
+     let history = useNavigate();
+
+     const [token,setToken] = useLocalStorage('token');
+
+     const [userLogin, setUserLogin] = useState<UserLogin>({
+         id: 0,
+         nome: '', 
+         usuario: '', 
+         senha: '',
+         foto: '',
+         nickname:'',
+         tipo:0,
+         token: ''
+     });
+
+     function updateModel(e:ChangeEvent<HTMLInputElement>){
+         setUserLogin({ 
+             ...userLogin,
+             [e.target.name]: e.target.value
+         })
+     }
+
+     useEffect(()=>{
+         if(token != ''){
+             history('/home')
+         }
+     },[token])
+ 
+ 
+     async function onSubmit(e:ChangeEvent<HTMLFormElement>) {
+         e.preventDefault();
+ 
+         try{
+            
+            await login (`/usuarios/logar`,userLogin,setToken)
+             
+             alert('Usuário logado com sucesso')
+         }catch(error){
+ 
+             alert('Dados do usuário incorretos, erro ao logar');
+         }
+     }
+
     return (
 
         <Grid container direction='row' justifyContent='center' alignItems='center' className='bg-color'>
 
             <Grid item alignItems='center' xs={6} >
                 <Box paddingX={10} className='login-top' >
-                    <form>
+                    <form onSubmit={onSubmit}>
                         <Typography variant="h6" className='texto1'>Bem Vindo</Typography>
 
                         <Typography className='texto0'>E-mail ou telefone </Typography>
-                        <TextField id='usuario' variant='outlined' margin='normal' name='Email ' fullWidth className='bordas' />
+                        <TextField value={userLogin.usuario} onChange={(e:ChangeEvent<HTMLInputElement>) => updateModel(e)} id='usuario' variant='outlined' margin='normal' name='usuario' fullWidth className='bordas' />
 
                         <Typography className='texto0'>Senha</Typography>
-                        <TextField id='senha' type='password' variant='outlined' margin='normal' fullWidth className='bordas' />
+                        <TextField value={userLogin.senha} onChange={(e:ChangeEvent<HTMLInputElement>) => updateModel(e)} id='senha' type='password' variant='outlined' margin='normal' fullWidth className='bordas' name='senha'/>
 
-                        <Link to='/home' style={{ textDecoration: 'none' }}>
                             <Box textAlign='center' marginTop={2}>
                                 <Typography variant="h6" className='texto2' style={{ fontWeight: 'bold' }}>Esqueceu a senha? </Typography>
                                 <Button type='submit' className='buttom'>  Entrar
                                 </Button>
                             </Box>
-                        </Link>
                     </form>
 
                     <Box display='flex' justifyContent='left' marginTop={2}>
