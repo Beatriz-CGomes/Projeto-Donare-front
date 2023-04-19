e1429ee6c820a153d3e9f35a49ff21de9be27d7e
joosilva <joaovictormendessilva1@gmail.com>
2023-04-19 01:50:41 -0300

    Página de cadastro.

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -13,30 +13,32 @@ function Login() {
             <Grid item alignItems='center' xs={6} >
                 <Box paddingX={10} className='login-top' >
                     <form>
-                       <Typography variant="h6" className='texto1'>Bem Vindo</Typography> 
-                       
-                        <Typography  className='texto0'>E-mail ou telefone </Typography>
-                        <TextField id='usuario'  variant='outlined' margin='normal'  name='Email ' fullWidth  className='bordas'/>
-                        
-                        <Typography  className='texto0'>Senha</Typography>
-                        <TextField id='senha'  type='password' variant='outlined' margin='normal' fullWidth   className='bordas' />
-                       
-                       <Link to='/home' style={{textDecoration:'none'}}>
-                        <Box textAlign='center' marginTop={2}>
-                            <Typography variant="h6" className='texto2' style={{ fontWeight: 'bold' }}>Esqueceu a senha? </Typography>
-                            <Button type='submit' className='buttom'>  Entrar
-                            </Button>
-                        </Box>
+                        <Typography variant="h6" className='texto1'>Bem Vindo</Typography>
+
+                        <Typography className='texto0'>E-mail ou telefone </Typography>
+                        <TextField id='usuario' variant='outlined' margin='normal' name='Email ' fullWidth className='bordas' />
+
+                        <Typography className='texto0'>Senha</Typography>
+                        <TextField id='senha' type='password' variant='outlined' margin='normal' fullWidth className='bordas' />
+
+                        <Link to='/home' style={{ textDecoration: 'none' }}>
+                            <Box textAlign='center' marginTop={2}>
+                                <Typography variant="h6" className='texto2' style={{ fontWeight: 'bold' }}>Esqueceu a senha? </Typography>
+                                <Button type='submit' className='buttom'>  Entrar
+                                </Button>
+                            </Box>
                         </Link>
                     </form>
 
                     <Box display='flex' justifyContent='center' marginTop={2}>
                         <Box marginRight={1}>
                             <Typography variant='subtitle1' gutterBottom align='center' className='chamada1'>Não tem uma conta? </Typography>
-                            </Box>
-                            <Typography variant='subtitle1' gutterBottom align='center' className='chamada2'>Cadastra-se </Typography>
                         </Box>
-                 
+                        <Link to="/cadastrar" style={{ textDecoration: "none" }}>
+                            <Typography variant='subtitle1' gutterBottom align='center' className='chamada2'>Cadastra-se </Typography>
+                        </Link>
+                    </Box>
+
                 </Box>
             </Grid>
             <Grid xs={6} className='imagem'>
