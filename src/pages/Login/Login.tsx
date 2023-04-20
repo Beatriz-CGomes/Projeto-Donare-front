eb305d8a7cf5d2041b72f74b54612cd5d986508b
Beatriz-CGomes <Beatriz.cg15@hotmail.com>
2023-04-19 23:27:51 -0300

    Página de Login, Ok

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,8 +1,9 @@
 import React, { ChangeEvent, useEffect, useState } from 'react';
 import { Grid, Typography, TextField, Button } from '@material-ui/core';
 import { Box } from '@mui/material';
-import { Link,useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { login } from '../../services/Service';
+import GoogleIcon from '@mui/icons-material/Google';
 
 import useLocalStorage from 'react-use-localstorage';
 import UserLogin from '../../models/UserLogin';
@@ -11,48 +12,48 @@ import './Login.css';
 
 
 function Login() {
-     let history = useNavigate();
+    let history = useNavigate();
 
-     const [token,setToken] = useLocalStorage('token');
+    const [token, setToken] = useLocalStorage('token');
 
-     const [userLogin, setUserLogin] = useState<UserLogin>({
-         id: 0,
-         nome: '', 
-         usuario: '', 
-         senha: '',
-         foto: '',
-         nickname:'',
-         tipo:0,
-         token: ''
-     });
+    const [userLogin, setUserLogin] = useState<UserLogin>({
+        id: 0,
+        nome: '',
+        usuario: '',
+        senha: '',
+        foto: '',
+        nickname: '',
+        tipo: 0,
+        token: ''
+    });
 
-     function updateModel(e:ChangeEvent<HTMLInputElement>){
-         setUserLogin({ 
-             ...userLogin,
-             [e.target.name]: e.target.value
-         })
-     }
+    function updateModel(e: ChangeEvent<HTMLInputElement>) {
+        setUserLogin({
+            ...userLogin,
+            [e.target.name]: e.target.value
+        })
+    }
 
-     useEffect(()=>{
-         if(token != ''){
-             history('/home')
-         }
-     },[token])
- 
- 
-     async function onSubmit(e:ChangeEvent<HTMLFormElement>) {
-         e.preventDefault();
- 
-         try{
-            
-            await login (`/usuarios/logar`,userLogin,setToken)
-             
-             alert('Usuário logado com sucesso')
-         }catch(error){
- 
-             alert('Dados do usuário incorretos, erro ao logar');
-         }
-     }
+    useEffect(() => {
+        if (token != '') {
+            history('/home')
+        }
+    }, [token])
+
+
+    async function onSubmit(e: ChangeEvent<HTMLFormElement>) {
+        e.preventDefault();
+
+        try {
+
+            await login(`/usuarios/logar`, userLogin, setToken)
+
+            alert('Usuário logado com sucesso')
+        } catch (error) {
+
+            alert('Dados do usuário incorretos, erro ao logar');
+        }
+    }
 
     return (
 
@@ -60,37 +61,62 @@ function Login() {
 
             <Grid item alignItems='center' xs={6} >
                 <Box paddingX={10} className='login-top' >
+
                     <form onSubmit={onSubmit}>
-                        <Typography variant="h6" className='texto1'>Bem Vindo</Typography>
+                        <Typography variant="h6" className='texto1'>Conecte-se</Typography>
+
 
                         <Typography className='texto0'>E-mail ou telefone </Typography>
-                        <TextField value={userLogin.usuario} onChange={(e:ChangeEvent<HTMLInputElement>) => updateModel(e)} id='usuario' variant='outlined' margin='normal' name='usuario' fullWidth className='bordas' />
+                        <TextField className='input-form' value={userLogin.usuario} onChange={(e: ChangeEvent<HTMLInputElement>) => updateModel(e)} id='usuario' variant='outlined' margin='normal' name='usuario' fullWidth />
+
 
                         <Typography className='texto0'>Senha</Typography>
-                        <TextField value={userLogin.senha} onChange={(e:ChangeEvent<HTMLInputElement>) => updateModel(e)} id='senha' type='password' variant='outlined' margin='normal' fullWidth className='bordas' name='senha'/>
+                        <TextField className='input-form' value={userLogin.senha} onChange={(e: ChangeEvent<HTMLInputElement>) => updateModel(e)} id='senha' type='password' variant='outlined' margin='normal' fullWidth name='senha' />
 
-                            <Box textAlign='center' marginTop={2}>
-                                <Typography variant="h6" className='texto2' style={{ fontWeight: 'bold' }}>Esqueceu a senha? </Typography>
-                                <Button type='submit' className='buttom'>  Entrar
-                                </Button>
-                            </Box>
+
+
+                        <Box textAlign='center' marginTop={2}>
+                            <Typography variant="h6" className='texto2' style={{ fontWeight: 'bold' }}>Esqueceu a senha? </Typography>
+                            <Button type='submit' className='buttom'>  Entrar
+                            </Button>
+                        </Box>
+
+
                     </form>
 
-                    <Box display='flex' justifyContent='left' marginTop={2}>
 
-                        <Typography variant='subtitle1' gutterBottom align='center' className='chamada1'>Não tem uma conta? </Typography>
+                    <Box display='flex' justifyContent='left' marginTop={3}>
+
+                        <Box>
+                            <Typography variant='subtitle1' gutterBottom align='center' className='chamada1'>Não tem uma conta? </Typography>
+                        </Box>
+
+
+                        <Link to="/cadastrar" style={{ textDecoration: "none" }}>
+                            <Typography variant='subtitle1' gutterBottom align='center' className='chamada2'>Cadastra-se </Typography>
+                        </Link>
                     </Box>
-                    <Link to="/cadastrar" style={{ textDecoration: "none" }}>
-                        <Typography variant='subtitle1' gutterBottom align='center' className='chamada2'>Cadastra-se </Typography>
-                    </Link>
+
+                    <Box display='flex' justifyContent='left' marginTop={1}>
+                        <Typography className='hr'> <hr></hr></Typography>
+                    </Box>
+                    <Box>
+                        <Typography className='hr'> <hr></hr></Typography>
+                    </Box>
+
+
+
+                    <Box display='flex' justifyContent='left' alignItems="center"  className='fundo'>
+                        <GoogleIcon className='redes' />
+                        <Typography gutterBottom align='center' className='google' >Continuar com o Google </Typography>
+
+                    </Box>
+
                 </Box>
-
-
             </Grid>
-            <Grid xs={3} className='imagem'>
-
+            <Grid xs={4} className='imagem'>
             </Grid>
-        </Grid>
+        </Grid >
 
 
 
