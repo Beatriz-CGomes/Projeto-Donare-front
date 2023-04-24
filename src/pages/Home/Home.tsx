100e2daa849bf8c3bd14129c30ac67b0ae54745d
JoasChoque <joaschoque@hotmail.com>
2023-04-23 21:54:08 -0300

    Estilização Home

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,20 +1,21 @@
 import React from "react";
-import { Avatar, Card, CardActions, CardContent} from "@material-ui/core";
+import { Avatar, Card, CardActions, CardContent } from "@material-ui/core";
 import { Collapse, Grid, IconButton, Paper, Typography } from "@material-ui/core";
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import ArrowForwardIcon from '@material-ui/icons/ArrowForward';
 
 import './Home.css'
-import { Box} from "@mui/material";
+import { Box } from "@mui/material";
+import { Link } from "react-router-dom";
 
 
 function Home() {
 
     //variaveis para a expansão do card
-    const [expanded,setExpanded] = React.useState(false)
+    const [expanded, setExpanded] = React.useState(false)
 
     //função para abrir e fechar o card
-    const clickExpandir = () =>{
+    const clickExpandir = () => {
         setExpanded(!expanded);
     }
 
@@ -34,18 +35,29 @@ function Home() {
                             </CardContent>
 
                             <CardActions className="box">
-                                <Typography className="padding-top-bot" variant="subtitle1">Mais opcões</Typography>
+                                <Typography className="padding-top-bot" >Mais opcões</Typography>
                                 <IconButton onClick={clickExpandir} aria-expanded={expanded} aria-label="show more">
                                     <ExpandMoreIcon />
                                 </IconButton>
                             </CardActions>
 
                             <Collapse in={expanded} timeout='auto' unmountOnExit>
-                                <CardContent>
-                                    <Typography className="padding-top-bot">Notícias</Typography>
-                                    <Typography className="padding-top-bot">ONGs</Typography>
-                                    <Typography className="padding-top-bot">Grupos</Typography>
-                                    <Typography className="padding-top-bot">Faça sua Doação!</Typography>
+                                <CardContent className="non-padding">
+                                    <Link to='#' className="clickable">
+                                        <Typography className="padding-top-bot padding-left clickable">Notícias</Typography>
+                                    </Link>
+
+                                    <Link to='#' className="clickable">
+                                    <Typography className="padding-top-bot padding-left clickable">ONGs</Typography>
+                                    </Link>
+
+                                    <Link to='#' className="clickable">
+                                    <Typography className="padding-top-bot padding-left clickable">Grupos</Typography>
+                                    </Link>
+
+                                    <Link to='#' className="clickable">
+                                    <Typography className="padding-top-bot padding-left clickable">Faça sua Doação!</Typography>
+                                    </Link>
                                 </CardContent>
                             </Collapse>
                         </Card>
@@ -57,56 +69,70 @@ function Home() {
                 </Grid>
 
                 <Grid item xs={3}>
-                   <Card variant="outlined" className="card2">
-                    <CardContent>
-                        <Typography variant="h6" className="titulo">Últimas notícias Sobre ONGs:</Typography>
+                    <Card variant="outlined" className="card2">
+                        <CardContent className="non-padding">
+                            <Typography variant="h6" className="titulo">Últimas notícias Sobre ONGs:</Typography>
 
-                        <Box className="padding-top-bot">
-                        <Typography variant="subtitle1">• Agência do Bem lança edital para seleção de ONGs ...</Typography>
-                        <Typography variant="subtitle2" >Data: 22/04/2023</Typography>
-                        </Box>
+                            <Box className="padding-top-bot padding-left clickable">
+                                <a href="https://www.jornaldebarueri.com.br/terceiro-setor/agencia-do-bem-lanca-edital-para-selecao-de-ongs-incluindo-barueri/"
+                                    target="_blank">
+                                    <Typography variant="subtitle1">• Agência do Bem lança edital para seleção de ONGs ...</Typography></a>
+                                    <Typography variant="subtitle2">Data: 22/04/2023</Typography>
+                                
+                            </Box>
 
-                        <Box className="padding-top-bot">
-                        <Typography variant="subtitle1">• Cadastro de ONGs para participar no Conselho...</Typography>
-                        <Typography variant="subtitle2" >Data: 18/04/2023</Typography>
-                        </Box>
+                            <Box className="padding-top-bot padding-left clickable">
+                                <a href="https://noticias.uol.com.br/ultimas-noticias/agencia-brasil/2023/04/23/ong-oferece-oficinas-de-criacao-artistica-para-pessoas-lgbtqia.htm"
+                                    target="_blank">
+                                    <Typography variant="subtitle1">• ONG oferece oficinas de criação artistica pra pessoas...</Typography></a>
+                                    <Typography variant="subtitle2" >Data: 18/04/2023</Typography>
+                                
+                            </Box>
 
-                        <Box className="padding-top-bot">
-                        <Typography variant="subtitle1">• Governo,Empresas e Ongs: Onde está a confiança...</Typography>
-                        <Typography variant="subtitle2" >Data: 29/04/2023</Typography>
-                        </Box>
+                            <Box className="padding-top-bot padding-left clickable">
+                                <a href="https://propmark.com.br/governo-empresas-e-ongs-onde-esta-a-confianca-dos-brasileiros/"
+                                    target="_blank">
+                                    <Typography variant="subtitle1">• Governo,Empresas e Ongs: Onde está a confiança...</Typography></a>
+                                    <Typography variant="subtitle2" >Data: 29/04/2023</Typography>
+                                
+                            </Box>
 
-                        <Box className="padding-top-bot">
-                        <Typography variant="subtitle1">• Conheça o trabalho de ONGs que protegem a fauna ...</Typography>
-                        <Typography variant="subtitle2">Data: 22/04/2023</Typography>
-                        </Box>
-                        
-                        <Box className="padding-top-bot">
-                        <Typography variant="subtitle1">• Parceria entre grandes ONGs e empreendedores ...</Typography>
-                        <Typography variant="subtitle2" >Data: 22/04/2023</Typography>
-                        </Box>
+                            <Box className="padding-top-bot padding-left clickable">
+                                <a href="https://www.metropoles.com/colunas/e-o-bicho/conheca-o-trabalho-de-ongs-que-protegem-a-fauna-do-cerrado-brasiliense"
+                                    target="_blank">
+                                    <Typography variant="subtitle1">• Conheça o trabalho de ONGs que protegem a fauna ...</Typography></a>
+                                    <Typography variant="subtitle2">Data: 22/04/2023</Typography>
+                            </Box>
 
-                        <Box className="padding-top-bot flex-row">
-                            <Typography className="margin-right-2">Ver mais notícias</Typography>
-                            <IconButton className="non-padding">
-                                <ArrowForwardIcon/>
-                            </IconButton>
-                        </Box>
-                    </CardContent>
-                   </Card>
+                            <Box className="padding-top-bot padding-left clickable">
+                                <a href="https://observatorio3setor.org.br/observatorio-em-movimento/parcerias-entre-grandes-ongs-e-empreendedores-sociais-independentes-ainda-sao-raras/"
+                                    target="_blank">
+                                    <Typography variant="subtitle1">• Parceria entre grandes ONGs e empreendedores ...</Typography></a>
+                                    <Typography variant="subtitle2">Data: 22/04/2023</Typography>
+                                
+                            </Box>
 
-                   <Card variant="outlined" className="card2">
-                    <CardContent>
-                        <Typography className="titulo">Recomendações de ONGs para seguir: </Typography>
-                        <ul>
-                            <li>WWF Brasil</li>
-                            <li>Green Peace</li>
-                            <li>Instituto Cuida de Mim</li>
-                            <li>Beija-Flores Solidários</li>
-                            <li>Amigos do bem</li>
-                        </ul>
-                    </CardContent>
-                   </Card>
+                            <Box className="padding-top-bot flex-row">
+                                <Typography className="margin-right-2 negrito">Ver mais notícias</Typography>
+                                <IconButton className="non-padding">
+                                    <ArrowForwardIcon />
+                                </IconButton>
+                            </Box>
+                        </CardContent>
+                    </Card>
+
+                    <Card variant="outlined" className="card2">
+                        <CardContent>
+                            <Typography className="titulo">Recomendações de ONGs para seguir: </Typography>
+                            <ul>
+                                <li>WWF Brasil</li>
+                                <li>Green Peace</li>
+                                <li>Instituto Cuida de Mim</li>
+                                <li>Beija-Flores Solidários</li>
+                                <li>Amigos do bem</li>
+                            </ul>
+                        </CardContent>
+                    </Card>
                 </Grid>
             </Grid>
         </>
