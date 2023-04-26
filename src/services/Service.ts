fa2647fa5aec43be79478c4b0065b73b3266d938
Beatriz-CGomes <beatriz.cg15@hotmail.com>
2023-04-26 10:07:39 -0300

    Service concluida

--- src/services/Service.ts
+++ src/services/Service.ts
@@ -14,3 +14,26 @@ export const login = async (url: any, dados: any, setDados: any) => {
     setDados(resposta.data.token)
 }
 
+export const buscar = async (url: any, setDados: any, header: any) => {
+    const resposta = await conexao.get(url, header)
+    setDados(resposta.data)
+}
+
+export const buscarId = async (url: any, setDados: any, header: any) => {
+    const resposta = await conexao.get(url, header)
+    setDados(resposta.data)
+}
+
+export const postar = async (url: any, dados: any, setDados: any, header: any) => {
+    const resposta = await conexao.post(url, dados, header)
+    setDados(resposta.data)
+}
+
+export const atualizar = async (url: any, dados: any, setDados: any, header: any) => {
+    const resposta = await conexao.put(url, dados, header)
+    setDados(resposta.data)
+}
+
+export const deletarId = async (url: any, header: any) => {
+    await conexao.delete(url, header)
+}
