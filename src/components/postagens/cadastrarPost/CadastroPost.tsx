2aee7320fba1ef5d5c9e3a9bf69ed5012f3974cb
GabiSilvaBelo <gabybelosp@gmail.com>
2023-04-28 12:13:10 -0300

    Cadastro da Postagem

--- src/components/postagens/cadastrarPost/CadastroPost.tsx
+++ src/components/postagens/cadastrarPost/CadastroPost.tsx
@@ -1,7 +1,140 @@
-import { FormControl, InputLabel, Select, TextField, Typography, MenuItem, Container, FormHelperText } from "@material-ui/core";
-import React from "react";
+import React, { ChangeEvent, useEffect, useState } from 'react'
+import { Container, Typography, TextField, Button, Select, InputLabel, MenuItem, FormControl, FormHelperText } from "@material-ui/core";
+import './CadastroPost.css';
+import { useNavigate, useParams } from 'react-router-dom';
+import Tema from '../../../models/Tema';
+import Postagem from '../../../models/Postagem';
+import { buscar, buscarId, postar, atualizar } from '../../../services/Service';
+import { useSelector } from 'react-redux';
+import { TokenState } from './../../../store/tokens/TokensReducer';
+import { toast } from 'react-toastify';
 
 function CadastroPost() {
+    let navigate = useNavigate();
+    const { id } = useParams<{ id: string }>();
+    const [temas, setTemas] = useState<Tema[]>([])
+    const token = useSelector<TokenState, TokenState["tokens"]>(
+        (state) => state.tokens
+      );
+
+    useEffect(() => {
+        if (token == "") {
+            toast.error('Você precisa estar logado', {
+                position: "top-right",
+                autoClose: 2000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: false,
+                draggable: false,
+                theme: "colored",
+                progress: undefined,
+            });
+            navigate("/login")
+
+        }
+    }, [token])
+
+    const [tema, setTema] = useState<Tema>(
+        {
+            id: 0,
+            nome: '',
+            descricao: ''
+        })
+    const [postagem, setPostagem] = useState<Postagem>({
+        id: 0,
+        titulo: '',
+        texto: '',
+        curtidas: null,
+        imagem:'',
+        marcacao:'',
+        doacao: '',
+        tema: null,
+    })
+
+    useEffect(() => { 
+        setPostagem({
+            ...postagem,
+            tema: tema
+        })
+    }, [tema])
+
+    useEffect(() => {
+        getTemas()
+        if (id !== undefined) {
+            findByIdPostagem(id)
+        }
+    }, [id])
+
+    async function getTemas() {
+        await buscar("/temas", setTemas, {
+            headers: {
+                'Authorization': token
+            }
+        })
+    }
+
+    async function findByIdPostagem(id: string) {
+        await buscarId(`postagens/${id}`, setPostagem, {
+            headers: {
+                'Authorization': token
+            }
+        })
+    }
+
+    function updatedPostagem(e: ChangeEvent<HTMLInputElement>) {
+
+        setPostagem({
+            ...postagem,
+            [e.target.name]: e.target.value,
+            tema: tema
+        })
+
+    }
+
+    async function onSubmit(e: ChangeEvent<HTMLFormElement>) {
+        e.preventDefault()
+
+        if (id !== undefined) {
+            atualizar(`/postagens`, postagem, setPostagem, {
+                headers: {
+                    'Authorization': token
+                }
+            })
+            toast.success('Postagem atualizada com sucesso', {
+                position: "top-right",
+                autoClose: 2000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: false,
+                draggable: false,
+                theme: "colored",
+                progress: undefined,
+            });
+        } else {
+            postar(`/postagens`, postagem, setPostagem, {
+                headers: {
+                    'Authorization': token
+                }
+            })
+            toast.success('Postagem cadastrada com sucesso', {
+                position: "top-right",
+                autoClose: 2000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: false,
+                draggable: false,
+                theme: "colored",
+                progress: undefined,
+            });
+        }
+        back()
+
+    }
+
+    function back() {
+        navigate('/postagens')
+    }
+    
     return (
         <>
             <Container maxWidth="xl" >
@@ -11,6 +144,7 @@ function CadastroPost() {
                     <TextField  id="titulo" name="titulo" label="Digite o titulo da sua postagem" variant="outlined" margin="normal" fullWidth />
                     <TextField  id="texto" name="texto" label="Digite o texto da sua postagem" variant="outlined"  margin="normal" fullWidth />
                     <TextField  id="foto" name="foto" label="Insira o link da foto em nuvem" variant="outlined"  margin="normal" fullWidth />
+                    <TextField  id="marcacao" name="marcacao" label="Marque um usuário aqui" variant="outlined"  margin="normal" fullWidth />
 
                     <FormControl>
                         <InputLabel id="demo-simple-select-helper-label">ODS </InputLabel>
