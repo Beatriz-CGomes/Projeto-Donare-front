88627e718e915d300ae7f5cbb2032bd1d5b3acfd
GabiSilvaBelo <gabybelosp@gmail.com>
2023-05-02 11:53:45 -0300

    Estilizando lista temas

--- src/components/temas/listaTemas/ListaTemas.tsx
+++ src/components/temas/listaTemas/ListaTemas.tsx
@@ -1,4 +1,4 @@
-import { Button, Card, CardActions, CardContent, Typography } from "@material-ui/core";
+import { Button, Card, CardActions, CardContent, Fab, Typography } from "@material-ui/core";
 import { Box } from "@mui/material";
 import { Link, useNavigate } from "react-router-dom"
 import React, { useEffect, useState } from "react";
@@ -7,18 +7,20 @@ import { buscar } from "../../../services/Service";
 import { useSelector } from "react-redux";
 import { TokenState } from "../../../store/tokens/TokensReducer";
 import { toast } from "react-toastify";
+import './ListaTemas.css'
+import AddIcon from '@material-ui/icons/Add';
 
 function ListaTemas() {
-    const [temas, setTemas] = useState <Tema[]>([])
+    const [temas, setTemas] = useState<Tema[]>([])
     let navigate = useNavigate()
 
     const token = useSelector<TokenState, TokenState["tokens"]>(
         (state) => state.tokens
     );
 
-    
-    useEffect(()=>{
-        if(token === "") {
+
+    useEffect(() => {
+        if (token === "") {
             toast.info('Você precisa estar logado para ter acesso a esse conteúdo', {
                 position: "top-right",
                 autoClose: 3000,
@@ -28,59 +30,82 @@ function ListaTemas() {
                 draggable: true,
                 progress: undefined,
                 theme: "colored",
-              });
+            });
             navigate("/login")
         }
-    },[token])
+    }, [token])
 
     async function buscaTema() {
         await buscar(`/temas`, setTemas, {
-        headers: {
-            "Authorization": token
-        }    
-        } )
+            headers: {
+                "Authorization": token
+            }
+        })
     }
 
-    useEffect (()=>{
+    useEffect(() => {
         buscaTema()
-    },[temas.length])
+    }, [temas.length])
     return (
         <>
-        {temas.map(tema=>(
-            <Box m={2} className="tamanho">
-                <Card variant="outlined">
-                    <CardContent>
-                        <Typography variant="h3" color="textSecondary" gutterBottom>
-                            {tema.nome}
-                        </Typography>
+            <Box className="imagem-ODS">
+                <img src="https://i.imgur.com/a0bqat1.png" width='100px' height='100px' />
+            </Box>
+            <Typography variant="h4" color="textSecondary" className="text-align" gutterBottom>
+                O que são as ODS?
+            </Typography>
 
-                        <Typography variant="h5" component='h2'>
-                            {tema.descricao}
-                        </Typography>
-                    </CardContent>
-                    <CardActions>
-                        <Box display='flex' justifyContent='center' mb={1.5}>
+            <Typography variant="subtitle1" color="textSecondary" className="text-align" gutterBottom>
+                OBJETIVOS DE DESENVOLVIMENTO SUSTENTÁVEL DA ONU
+            </Typography>
 
-                            <Link to={`/formTema/${tema.id}`} className="text-decorator none">
-                                <Box mx={1}>
-                                    <Button variant="contained" className="marginLeft" size="small" color="primary">
-                                        Atualizar
-                                    </Button>
-                                </Box>
-                            </Link>
+            <Typography variant="body1" className="text-align">
+                Em 2015, a ONU propôs aos seus países membros uma nova agenda de desenvolvimento sustentável para os próximos 15 anos, a Agenda 2030, composta pelos 17 Objetivos de Desenvolvimento Sustentável (ODS).
 
-                            <Link to={`/deletarTema/${tema.id}`} className="text-decorator none">
-                                <Box mx={1}>
-                                    <Button variant="contained" size="small" color="secondary">
-                                        Deletar
-                                    </Button>
-                                </Box>
-                            </Link>
-                        </Box>
-                    </CardActions >
-                </Card >
-            </Box >
+                Esse é um esforço conjunto, de países, empresas, instituições e sociedade civil. Os ODS buscam assegurar os direitos humanos, acabar com a pobreza, lutar contra a desigualdade e a injustiça, alcançar a igualdade de gênero e o empoderamento de mulheres e meninas, agir contra as mudanças climáticas, bem como enfrentar outros dos maiores desafios de nossos tempos. O setor privado tem um papel essencial nesse processo como grande detentor do poder econômico, propulsor de inovações e tecnologias influenciador e engajador dos mais diversos públicos – governos, fornecedores, colaboradores e consumidores. (Fonte: Pacto Global Rede Brasil)
+            </Typography>
+
+            {temas.map(tema => (
+                <Box m={2} className="tamanho">
+                    <Card variant="outlined">
+                        <CardContent>
+                            <Typography variant="h3" color="textSecondary" gutterBottom>
+                                {tema.nome}
+                            </Typography>
+
+                            <Typography variant="h5" component='h2'>
+                                {tema.descricao}
+                            </Typography>
+                        </CardContent>
+                        <CardActions>
+                            <Box display='flex' justifyContent='center' mb={1.5}>
+
+                                <Link to={`/formTema/${tema.id}`} className="text-decorator none">
+                                    <Box mx={1}>
+                                        <Button variant="contained" className="marginLeft" size="small" color="primary">
+                                            Atualizar
+                                        </Button>
+                                    </Box>
+                                </Link>
+
+                                <Link to={`/deletarTema/${tema.id}`} className="text-decorator none">
+                                    <Box mx={1}>
+                                        <Button variant="contained" size="small" color="secondary">
+                                            Deletar
+                                        </Button>
+                                    </Box>
+                                </Link>
+                            </Box>
+                        </CardActions >
+                    </Card >
+                </Box >
+
             ))}
+            <Link to='/formTema'>
+            <Fab color="primary" aria-label="add" className="buttonAdd">
+                <AddIcon />
+            </Fab>
+            </Link>
         </>
     )
 }
